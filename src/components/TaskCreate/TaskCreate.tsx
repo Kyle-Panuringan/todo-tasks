5f02cc86cb325ffc1ee1ac053623466333e8650c
Kyle <kylepanurignan@gmail.com>
2023-08-17 19:12:43 +0900

    TaskCreate: Add Button X clear functionality

--- src/components/TaskCreate/TaskCreate.tsx
+++ src/components/TaskCreate/TaskCreate.tsx
@@ -1,11 +1,33 @@
+import { useState } from "react";
 import "./taskCreate.scss";
 
 const TaskCreate = () => {
+  const [taskCreateValue, setTaskCreateValue] = useState("");
+
+  const handleTaskCreateValueClear = () => {
+    setTaskCreateValue("");
+  };
+
   return (
     <form id="taskCreate">
       <div id="taskCreateInputBox">
-        <input type="text" name="taskInputSearch" id="taskInputSearch" />
-        <button type="button">X</button>
+        <input
+          type="text"
+          name="taskInputSearch"
+          id="taskInputSearch"
+          placeholder="Enter Task..."
+          value={taskCreateValue}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setTaskCreateValue(e.target.value)
+          }
+        />
+        <button
+          type="button"
+          style={{ visibility: `${taskCreateValue ? "visible" : "hidden"}` }}
+          onClick={handleTaskCreateValueClear}
+        >
+          X
+        </button>
       </div>
       <button type="submit" id="taskAddCreateButton">
         Add
